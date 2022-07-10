e3e49ad058d1978f61f4296a5a847af5c5cb967d
ABHISHEK SINGH <abhiswee@gmail.com>
2022-07-10 16:59:39 -0400

    adding serial comm

--- server/app.js
+++ server/app.js
@@ -1,17 +1,34 @@
 const express = require('express');
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+
 
 const app = express();
+
+
 const PORT = 3000;
 app.use(express.json());
 app.post('/', (req, res)=>{
 	const {name} = req.body;
+
 	res.send(`Welcome ${name}`);
-})
+});
+
+app.get('/ports', (req, res)=>{
+	
+	
+	  //ports => ports.forEach(port =>console.log(port.path)),
+	  res.send(`ports`);
+});
+
+
 
 app.listen(PORT, (error) =>{
 	if(!error)
+	{
+		console.log(serialport.SerialPort.length);
 		console.log("Server is Successfully Running, and App is listening on port "+ PORT);
-	else
+	}else
 		console.log("Error occurred, server can't start", error);
 	}
 );
