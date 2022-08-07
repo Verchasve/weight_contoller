c5052e64a39ba22d7b4438b76640d8a585dfaeaf
ABHISHEK SINGH <abhiswee@gmail.com>
2022-08-07 09:24:30 -0400

    commit

--- server/app.js
+++ server/app.js
@@ -1,32 +1,37 @@
 const express = require('express');
-var serialport = require("serialport");
-var SerialPort = serialport.SerialPort;
-
+const serialport = require("serialport");
+const SerialPort = serialport.SerialPort;
+//const bindings = require('@serialport/bindings');
+var listOfPorts=[];
 
 const app = express();
-
-
 const PORT = 3000;
+
 app.use(express.json());
+
 app.post('/', (req, res)=>{
 	const {name} = req.body;
-
 	res.send(`Welcome ${name}`);
 });
 
 app.get('/ports', (req, res)=>{
-	
-	
-	  //ports => ports.forEach(port =>console.log(port.path)),
 	  res.send(`ports`);
-});
-
-
+}); 
+ 
 
 app.listen(PORT, (error) =>{
 	if(!error)
 	{
-		console.log(serialport.SerialPort.length);
+ //called automatically by bindings.list()
+function list(ports) {
+	listOfPorts=ports;
+	// now listOfPorts will be the port Objects
+	console.log(listOfPorts);
+  }
+  
+  //bindings.list().then(list, err => {
+	process.exit(1)
+  //})
 		console.log("Server is Successfully Running, and App is listening on port "+ PORT);
 	}else
 		console.log("Error occurred, server can't start", error);
